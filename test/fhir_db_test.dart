71d6c65fb8b9b4d4222ab4462abc55fb7de2662e
Dokotela <jgfaulkenberry@gmail.com>
2020-09-16 15:14:27 -0400

    Updating, should stand on it's own

--- test/fhir_db_test.dart
+++ test/fhir_db_test.dart
@@ -1,18 +1,1 @@
-import 'package:fhir/fhir_r4.dart';
-import 'package:fhir_db/resource_dao.dart';
-
-void main() async {
-  ResourceDao resourceDao = ResourceDao('Observation');
-
-  for (int i = 0; i < 10; i++) {
-    Observation observation = Observation(
-        id: Id(
-            DateTime.now().toString().replaceAll(':', '').replaceAll(' ', '')),
-        resourceType: 'Observation',
-        code: CodeableConcept(text: 'newObservation'));
-
-    await resourceDao.insert(observation);
-  }
-  var resources = await resourceDao.getAllSortedById();
-  resources.forEach((resource) => print(resource.toJson()));
-}
+void main() async {}
